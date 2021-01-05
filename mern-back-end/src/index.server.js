18871c55f2a8fb577543b181d55f3bd25f5a7559
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-01-06 01:21:18 +0530

    nodemon was installed

--- mern-back-end/src/index.server.js
+++ mern-back-end/src/index.server.js
@@ -11,6 +11,12 @@ app.get('/',(req,res,next)=>{
     })
 })
 
+app.post('/data',(req,res,next)=>{
+    res.status(200).json({
+        message:req.body
+    })
+})
+
 app.listen(process.env.PORT,()=>{
     console.log(`app is running on PORT ${process.env.PORT}`);
 });
