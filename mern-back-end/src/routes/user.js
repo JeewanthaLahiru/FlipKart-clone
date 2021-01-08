686cefb6b2a44ff75264dc499c86fb89c0f55f8f
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-01-08 19:16:24 +0530

    added some apis in user.js in routes

---
+++ mern-back-end/src/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/signin',(req,res)=>{
+
+});
+
+router.post('/signin',(req,res)=>{
+
+});
+
+module.exports = router;
