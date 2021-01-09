6af375cd2531849193e95fc1de4d2bea8885687d
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-01-10 01:57:39 +0530

    added profile endpoint

--- mern-back-end/src/routes/auth.js
+++ mern-back-end/src/routes/auth.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 
 router.post('/signup',signup);
-
 router.post('/signin',signin);
+
+router.post('/profile',(req,res)=>{
+    res.status(200).json({user:'profile'})
+})
 
 module.exports = router;
