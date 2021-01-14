58bd01fa63a8b4a35e78382effc2f1bcf134fdbf
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-01-15 00:14:21 +0530

    signup form

--- admin-app/src/containers/Signup/index.js
+++ admin-app/src/containers/Signup/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 
 /**
 * @author
@@ -9,7 +10,46 @@ import Layout from '../../components/Layout';
 const Signup = (props) => {
   return(
     <Layout>
-      <div>Signup</div>
+      <Container>
+        <Row style={{marginTop:'50px'}}>
+          <Col md={{span:6, offset:3}}>
+            <Form>
+
+              <Row>
+                <Col md={6}>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type="text" placeholder="First Name" />
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type="text" placeholder="Last Name" />
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+
+      </Container>
     </Layout>
    )
 
