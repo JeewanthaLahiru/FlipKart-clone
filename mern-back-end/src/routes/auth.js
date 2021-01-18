83e537da59eccc9b24cde75ade386c99e1a4bba0
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-01-18 21:54:47 +0530

    api validation array in signup

--- mern-back-end/src/routes/auth.js
+++ mern-back-end/src/routes/auth.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const { signup, signin, requireSignin } = require('../controller/auth');
+const { check } = require('express-validator');
 const router = express.Router();
 
 
-router.post('/signup',signup);
+router.post('/signup',[
+    check('firstName')
+    .isEmpty()
+    .withMessage('firstName is required'),
+    check('lastName')
+    .isEmpty()
+    .withMessage('lastName is required'),
+    check('email')
+    .isEmail()
+    .withMessage('Valid Email is required'),
+    check('password')
+    .isLength({min:6})
+    .withMessage('Password must be at least 6 characters long')
+],signup);
 router.post('/signin',signin);
 
 //first requireSignin is called then req,res function is called because next() function
