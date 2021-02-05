e6d3f854163eece31693e08e8697ffe04e2602b2
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-02-05 22:34:38 +0530

    admin signup validation

--- mern-back-end/src/routes/admin/auth.js
+++ mern-back-end/src/routes/admin/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { signup, signin, requireSignin } = require('../../controller/admin/auth');
+const { validateSignupRequest, isRequestValidated } = require('../../validators/auth');
 const router = express.Router();
 
 
-router.post('/admin/signup',signup);
+router.post('/admin/signup',validateSignupRequest,isRequestValidated,signup);
 router.post('/admin/signin',signin);
 
 //first requireSignin is called then req,res function is called because next() function
