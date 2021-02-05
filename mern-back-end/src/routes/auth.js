ccd5efed9526e23bc0ec48611e6b4a1d6555f8e4
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-02-05 22:33:30 +0530

    imported validators to user signup

--- mern-back-end/src/routes/auth.js
+++ mern-back-end/src/routes/auth.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const { signup, signin, requireSignin } = require('../controller/auth');
 const { check } = require('express-validator');
+const { validateSignupRequest, isRequestValidated } = require('../validators/auth');
 const router = express.Router();
 
 
-router.post('/signup',[
-    check('firstName')
-    .notEmpty()
-    .withMessage('firstName is required'),
-    check('lastName')
-    .notEmpty()
-    .withMessage('lastName is required'),
-    check('email')
-    .isEmail()
-    .withMessage('Valid Email is required'),
-    check('password')
-    .isLength({min:6})
-    .withMessage('Password must be at least 6 characters long')
-],signup);
+router.post('/signup',validateSignupRequest,isRequestValidated,signup);
 router.post('/signin',signin);
 
 //first requireSignin is called then req,res function is called because next() function
