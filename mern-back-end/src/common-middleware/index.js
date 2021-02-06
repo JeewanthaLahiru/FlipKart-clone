d9489359abec06ff77422b8629bc5ebb6d5aed38
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-02-06 19:45:11 +0530

    geting user access

--- mern-back-end/src/common-middleware/index.js
+++ mern-back-end/src/common-middleware/index.js
@@ -14,12 +14,15 @@ exports.requireSignin = (req, res, next) =>{
 }
 
 exports.userMiddleware = (req,res,next)=>{
-
+    if(req.user.role !== 'user'){
+        return res.status(400).json({message:'User Access Denied'});
+    }
+    next();
 }
 
 exports.adminMiddleware = (req,res,next)=>{
     if(req.user.role !== 'admin'){
-        return res.status(400).json({message:'Access Denied'});
+        return res.status(400).json({message:'Admin Access Denied'});
     }
     next();
 }
