447be3797916f6f90ba3c63e84f76f8bd1ebff35
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-02-07 00:33:04 +0530

    multer was installed

---
+++ mern-back-end/src/routes/product.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { requireSignin, adminMiddleware } = require('../common-middleware');
+//const { addCategory, getCategories } = require('../controller/category');
+const router = express.Router();
+//const Product = require('../models/product');
+
+router.post("/product/create",requireSignin,adminMiddleware,(req,res)=>{
+    res.status(200).json({message:"Hello"});
+});
+//router.get("/product/getcategory",getCategories);
+
+module.exports = router;
