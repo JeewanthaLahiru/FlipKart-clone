f084d364088a6a640dde03692cce40dbfb6241a2
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-02-11 01:11:10 +0530

    check addtocart api

--- mern-back-end/src/index.server.js
+++ mern-back-end/src/index.server.js
@@ -8,6 +8,7 @@ const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use('/api',authRoutes);
 app.use('/api',adminRoutes);
 app.use('/api',categoryRoutes);
 app.use('/api',productRoutes);
+app.use('/api',cartRoutes);
 
 app.listen(process.env.PORT,()=>{
     console.log(`app is running on PORT ${process.env.PORT}`);
