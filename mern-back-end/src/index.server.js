7383c92dea6a462f1ad2acc5f77b886467c1cfef
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-02-12 00:07:02 +0530

    updated category api to upload images to category

--- mern-back-end/src/index.server.js
+++ mern-back-end/src/index.server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const env = require('dotenv');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 //routes
 const authRoutes = require('./routes/auth');
@@ -28,6 +29,7 @@ mongoose.connect(
         console.log('Database connected');
     });
 app.use(bodyParser.json());
+app.use('/public',express.static(path.join(__dirname, 'uploads')));
 app.use('/api',authRoutes);
 app.use('/api',adminRoutes);
 app.use('/api',categoryRoutes);
